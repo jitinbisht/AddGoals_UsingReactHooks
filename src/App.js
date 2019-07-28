35f470449cba4808cf86b22f2624d345dcf07789
Jitin Bisht <jitinbisht.bisht@gmail.com>
2019-07-29 01:29:28 +0530

    React Hooks project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [goals, setGoals] = useState([]);
+  //FETCH DATA USING REACT HOOKS
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("http://localhost:9000/goals");
+      res
+        .json()
+        .then(res => setGoals(res.data))
+        .catch(err => setGoals(err));
+    }
+    fetchData();
+  }, []);
+
+  //ADD ACTION
+  const addGoals = () => {
+    setGoals([...goals, { title: "", description: "" }]);
+  };
+
+  //SAVE ACTION
+  const [success, setMessage] = useState("");
+  const saveGoals = () => {
+    fetch("http://localhost:9000/goals", {
+      method: "post"
+    })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        console.log("SAving Goals...:", data);
+        setMessage(data);
+      });
+  };
+
+  //DELETE ACTION
+  const deleteData = (item, url = "http://localhost:9000/goals/") => {
+    console.log(item, "===deleting item===");
+    console.log(url, "===deleting URL===");
+    return fetch(url, {
+      headers: { "Content-Type": "application/json; charset=utf-8" },
+      method: "delete"
+    })
+      .then(response => response.json())
+      .then(function(data) {
+        console.log(data, "====deletiing goals====");
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="goalsContainer">
+      <form className="formContainer">
+        {goals ? (
+          goals.map(goal => (
+            <>
+              <div className="form-group inputTitle">
+                <i className="fa fa-remove icon deleteIcon" />
+                <input
+                  placeholder={goal.title}
+                  className="form-control"
+                  type="text"
+                />
+              </div>
+              <div class="form-group">
+                <textarea
+                  className="form-control rounded-0 inputDescription"
+                  placeholder={goal.description}
+                  rows="3"
+                />
+              </div>
+            </>
+          ))
+        ) : (
+          <div>No Data found...</div>
+        )}
+        <i className="glyphicon glyphicon-plus-sign addIcon" />
+        <button type="button" className="btn addButton " onClick={addGoals}>
+          Add a New Goal
+        </button>
+        <button type="button" className="btn saveButton" onClick={saveGoals}>
+          Save
+        </button>
+        {success.message && (
+          <div className="alert alert-success" role="alert">
+            <strong>{success.message} </strong>
+          </div>
+        )}
+      </form>
     </div>
   );
-}
-
+};
 export default App;
---
+++ src/Goals.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Goals = props => {
+  console.log(props.goals, "==inside goals===");
+};
+
+export default Goals;
